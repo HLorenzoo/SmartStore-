024d4a4999403a118b3117ae750fd9da88db5cd6
Hlorenzoo <hernanlorenzo97@hotmail.com>
2022-08-23 16:38:33 -0300

    Modelo de Usuario

---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  Provincia: {
+    type: String,
+    required: true,
+  },
+  Direccion: {
+    type: String,
+    required: true,
+  },
+  tel: {
+    type: Number,
+    required: true,
+  },
+  ordenes: {
+    type: Array,
+    default: [],
+  },
+  salt: {
+    type: String,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// Schema Hook => has de la password y creacion del salt del usuario
+UserSchema.pre("save", async function () {
+  this.salt = bcrypt.genSaltSync();
+  return (this.password = await bcrypt.hash(this.password, this.salt));
+});
+
+module.exports = mongoose.model("User", UserSchema);

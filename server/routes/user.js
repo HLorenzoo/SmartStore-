f4396f426278a0293128c151418da235bb62d68c
Hlorenzoo <hernanlorenzo97@hotmail.com>
2022-08-24 18:08:54 -0300

    Product model

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
-
-//GET
-router.get("/", UserController.getAllUser);
-
+const { verifyTokenAdmin } = require("../middelware/auth.middelware");
 //POST
 router.post("/ordenes/:_id", UserController.addProduct);
 router.post("/favoritos/:_id", UserController.addFav);
 
+// --- DEBERIA SER DE ADMIN
+//GET
+router.get("/", verifyTokenAdmin, UserController.getAllUser);
+
 //PUT
-router.put("/:_id", UserController.deleteUser);
+router.put("/:_id", verifyTokenAdmin, UserController.deleteUser);
 
 module.exports = router;

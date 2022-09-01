343a832d3ba85f33b9728f3bd61aaf7072e89592
LorenaPinkala <lorenapinkala77@gmail.com>
2022-09-01 16:52:27 -0300

    con la documentacion readme

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const client = mongoose
-  .connect(process.env.MONGO_URL)
+  .connect('mongodb+srv://SMART:SMART@cluster0.lje0d28.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log("Succesfully db connected"))
   .catch((err) => console.error(err));
 
